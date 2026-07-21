679e3e8d04d688f92d1882dffa7198ae2ab9cf1c
Paulo Santos <61875054+paolivsantos@users.noreply.github.com>
2026-07-20 21:44:45 -0300

    Update app.py
    Ajuste nomes

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ URL_PLANILHA = "https://docs.google.com/spreadsheets/d/10Ju9R5RylNF6HHK_oV-NBZij
 
 # Mapeamento consolidado
 MAPEAMENTO_RR = {
-    "Alescia Bezerra Fernandes": "84654", "Anderson Batista Da Costa": "85236", "Anderson Lazaro Gomes Oliva": "78172",
-    "Bruno Rafael Borges Beltrao Moiteiro": "84437", "Dyonathan Jordan Do Nascimento Araujo": "86281", "Gabriel Shioda Lima": "86618",
+    "Alescia Fernandes": "84654", "Anderson Batista Da Costa": "85236", "Anderson Lazaro Gomes Oliva": "78172",
+    "Bruno Rafael Borges Beltrao Moiteiro": "84437", "Dyonathan Jordan": "86281", "Gabriel Shioda Lima": "86618",
     "Glaucia Hiromi Mekaru": "84452", "Guilherme Oliver Barreira": "85338", "Jorge Luiz dos Santos": "86238",
     "José Clailton Menezes Jorge": "85479", "Lucas De Maria Godinho": "85224", "Lucas Martinez": "84515",
     "Rafael Ferreira Da Silva": "85051", "Reinaldo Marques": "82927", "Ronaldo de Souza Maciel": "84162",
